2209c8ed7cd1afa7540e04fd7dc6c84c6714730a
unknown <saadali.sheikh7865@example.com>
2020-04-15 09:37:06 +0500

    Updates

---
+++ precache-manifest.bc5f9877d3da42301f247bb622915851.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf1164af19beffdeaf97b617f6b1df1",
+    "url": "/Tik-Tac-Toe/index.html"
+  },
+  {
+    "revision": "7d68cf20a129185af3a6",
+    "url": "/Tik-Tac-Toe/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "cf61388fb6b611458c6e",
+    "url": "/Tik-Tac-Toe/static/css/main.109ac464.chunk.css"
+  },
+  {
+    "revision": "7d68cf20a129185af3a6",
+    "url": "/Tik-Tac-Toe/static/js/2.23f7c06e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tik-Tac-Toe/static/js/2.23f7c06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf61388fb6b611458c6e",
+    "url": "/Tik-Tac-Toe/static/js/main.5e7da8e5.chunk.js"
+  },
+  {
+    "revision": "dbc5f01833545ba06ea2",
+    "url": "/Tik-Tac-Toe/static/js/runtime-main.938befa7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Tik-Tac-Toe/static/media/SEGA.12a3db8b.woff"
+  }
+]);
